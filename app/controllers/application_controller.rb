440fa5d9dcae2a2b05bc70c01932bc8ce980047c
TEJINDER KALSI <singhtejinder414@gmail.com>
2020-09-13 19:09:47 +0530

    'changed_devise_default_routes'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 	include DeviseWhitelist
 
 def after_sign_in_path_for(resource)
-    root_path
+    home_path
 end
 
 def after_sign_out_path_for(resource)
