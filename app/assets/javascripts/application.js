ffb311975fa673a431c67fd7752e9a5a12ff2152
tejindersinghkalsi <singhtejinder414@gmail.com>
2021-06-04 10:41:38 +0530

    'Changes'

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,12 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require chartkick
-//= require Chart.bundle
+//= require jquery3
 //= require bootstrap-sprockets
 //= require rails-ujs
+//= require chartkick
+//= require Chart.bundle
 //= require activestorage
 //= require popper
 //= require cocoon
